90b95a90359fbc1719d5fefc9cb72f153ba349bd
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2024-04-18 20:54:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold uppercase">Expense Calculator</h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
